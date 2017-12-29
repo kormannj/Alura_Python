5def283d5ffd3a58adf0ada54e392b06555327ae
kormannj <33202837+kormannj@users.noreply.github.com>
2017-12-29 13:57:15 -0200

    Add files via upload

--- conta.py
+++ conta.py
@@ -3,16 +3,20 @@ class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
         print('Construindo objeto ... {}.'.format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print(f'O saldo da conta {self.numero} é {self.saldo}.')
+        print(f'O saldo da conta {self.__numero} é {self.__saldo}.')
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
+
+    def transfere(self, valor, conta_destino):
+        self.saca(valor)
+        conta_destino.deposita(valor)
